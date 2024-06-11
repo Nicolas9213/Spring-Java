66a5d0222ce74aa732f74fccb64a39a0e4747c88
Nicolas A. Gazaniga <nicolasgazaniga13@gmail.com>
2024-06-10 21:57:27 -0300

    Aula 10/06/2024

--- demo/src/main/java/br/senai/sc/demo/model/Usuario.java
+++ demo/src/main/java/br/senai/sc/demo/model/Usuario.java
@@ -1,18 +1,23 @@
 package br.senai.sc.demo.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
     
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // Indica a primary key da identidade
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Faz com que o banco gere um valor aleatório
     private Integer id;
     @Column (nullable = false)
+    @Getter
     private String nome;
     @Column (unique = true)
     private String email;
+    @ToString.Exclude
     @Column (name = "password")
     private String senha;
     @Column (length = 50)
